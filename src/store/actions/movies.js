b1e6002fbf68eca5af7086fe3bd9271bef9a4009
Jayonta Sarkar <jayontasarkar025@gmail.com>
2021-08-19 01:07:05 +0600

    prettify

--- src/store/actions/movies.js
+++ src/store/actions/movies.js
@@ -8,37 +8,35 @@ import {
   MOVIE_TYPE
 } from '../types';
 
-export const fetchMovies =
-  (type, pageNo = 1, loader = true) =>
-  async (dispatch) => {
-    try {
-      dispatch({ type: LOADING_MOVIES, payload: loader });
-      const {
-        data: { results, page, total_pages }
-      } = await getMovies(type, pageNo);
-      dispatch({ type: MOVIE_LIST, payload: { results, page, total_pages } });
-      dispatch({ type: LOADING_MOVIES, payload: false });
-    } catch (error) {
-      dispatch({ type: SET_ERROR, payload: error.message });
-      dispatch({ type: LOADING_MOVIES, payload: false });
-    }
-  };
+export const fetchMovies = (type, pageNo = 1, loader = true) => async (dispatch) => {
+  try {
+    dispatch({ type: LOADING_MOVIES, payload: loader });
+    const {
+      data: { results, page, total_pages }
+    } = await getMovies(type, pageNo);
+    dispatch({ type: MOVIE_LIST, payload: { results, page, total_pages } });
+    dispatch({ type: LOADING_MOVIES, payload: false });
+  } catch (error) {
+    dispatch({ type: SET_ERROR, payload: error.message });
+    dispatch({ type: LOADING_MOVIES, payload: false });
+  }
+};
 
 export const loadMoreMovies =
   (type, pageNo = 1) =>
-  async (dispatch) => {
-    try {
-      const {
-        data: { results, page, total_pages }
-      } = await getMovies(type, pageNo);
-      dispatch({
-        type: LOAD_MORE_RESULTS,
-        payload: { list: results, page, totalPages: total_pages }
-      });
-    } catch (error) {
-      dispatch({ type: SET_ERROR, payload: error.message });
-    }
-  };
+    async (dispatch) => {
+      try {
+        const {
+          data: { results, page, total_pages }
+        } = await getMovies(type, pageNo);
+        dispatch({
+          type: LOAD_MORE_RESULTS,
+          payload: { list: results, page, totalPages: total_pages }
+        });
+      } catch (error) {
+        dispatch({ type: SET_ERROR, payload: error.message });
+      }
+    };
 
 export const setResponsePageNumber = (page, totalPages) => async (dispatch) => {
   dispatch({ type: RESPONSE_PAGE, payload: { page, totalPages } });
